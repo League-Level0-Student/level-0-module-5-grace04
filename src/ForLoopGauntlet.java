1b52bdd007521ddb6033a59f3a60483f054b3bd9
league <league@wts21.attlocal.net>
2018-05-05 17:26:48 -0700

    l0m5

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,60 @@
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+		//for (int i=0;i<101;i++) {
+		//	System.out.println(i);
+		//}
+		
+		//for (int i=100;i>-1;i--) {
+		//	System.out.println(i);
+		//}
+		
+		//for (int i=2;i<102;i++) {
+		//	if (i%2==0) {
+		//	System.out.println(i);
+		//	}
+		//}
+		
+		//for (int i=1;i<100;i++) {
+		//	if (i%2==1) {
+		//	System.out.println(i);
+		//	}
+		//}
+		
+		//for (int i=1;i<102;i++) {
+		//	if (i%2==0) {
+		//		System.out.println(i + "even");
+		//	}
+		//	else if (i%2==1) {
+		//		System.out.println(i + "odd");
+		//	}
+		//}
+		
+		//for (int i=0;i<778;i++) {
+		//	if (i%7==0) {
+		//		System.out.println(i);
+		//	}
+		//}
+		
+		//int j;
+		//for (int i=0;i<14;i++) {
+		//	System.out.println(i+" years old");
+		//	j=i+2004;
+		//	System.out.println("in " + j);
+		//}
+		
+		//for (int i=0;i<3;i++) {
+		//	for (int j=0;j<3;j++) {
+		//		System.out.println(i + " " + j);
+		//	}
+		//}
+		
+		//for (int i=1;i<8;i++) {
+		//	for (int j=2;j<9;j++) {
+		//		for (int k=3;k<10;k++) {
+		//			System.out.println(i+" "+j+" "+k);
+		//		}
+		//	}
+		//}
+	}
+}
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -7,23 +7,34 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+	String Pi = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
-		//    Run your program to see how this works.
-
+		//    Run your program to see how this work.
+	int x;
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
 
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-
+	for (int i=0;i<Pi.length();i++) {
+		System.out.println(Pi.charAt(i));
+		String Ans = JOptionPane.showInputDialog("What's the next digit of Pi?");
+		x = Ans.charAt(0);
+		if (x==Pi.charAt(i+1)){
+			JOptionPane.showMessageDialog(null, "You are correct!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You are incorrect.");
+			break;
+		}
+	}
 				// 5. Ask the user for the NEXT digit of pi.
-
+	
 				// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
@@ -32,7 +43,7 @@ public class GuessingPi {
 
 					// 8. If they are not, print out "incorrect" and tell them
 					//    to start over. Use 'break;' to break out of the loop.
-
+	}
 }
 
 
